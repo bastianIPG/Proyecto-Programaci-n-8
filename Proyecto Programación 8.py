8015dfbee9558de69058c898ea6e1f830c949cbe
Yerko-Yuivar <86970750+Yerko-Yuivar@users.noreply.github.com>
2021-07-05 18:16:25 -0400

    Create Proyecto Programación 8.py

---
+++ Proyecto Programación 8.py
@@ -0,0 +1,38 @@
+flag = True
+while flag:
+    option = input("[1] Ingrese el nombre de la region: \n")
+    if option == "1" or option == "Tarapaca":
+        print("Region de Tarapaca")
+    if option == "2" or option == "Antofagasta":
+        print("Region de Antofagasta")
+    if option == "3" or option == "Atacama":
+        print("Region de Atacama")
+    if option == "4" or option == "Coquimbo":
+        print("Region de Coquimbo")
+    if option == "5" or option == "Valparaiso":
+        print("Region de Valparaiso")
+    if option == "6" or option == "Bernardo Ohiggins":
+        print("Region General Bernardo Ohiggins")
+    if option == "7" or option == "Maule":
+        print("Region del Maule")
+    if option == "8" or option == "Biobío":
+        print("Region del Biobío")
+    if option == "9" or option == "Araucania":
+        print("Region de la Araucania")
+    if option == "10" or option == "Los lagos":
+        print("Region de Los lagos")
+    if option == "11" or option == "Aysen":
+        print("Region de Aysen")
+    if option == "12" or option == "Magallanes":
+        print("Region de Magallanes")
+    if option == "13" or option == "Region Metropolitana":
+        print("Region Metropolitana")
+    if option == "14" or option == "Los rios":
+        print("Region de Los rios")
+    if option == "15" or option == "Arica y parinacota":
+        print("Region de Arica y parinacota")
+    if option == "16" or option == "Ñuble":
+        print("Region del Ñuble")
+    elif option == "0":
+        flag = False
+print("Saliendo del programa....")
